5b1ecb93cc9c2553065c8bed139a4f2a16382b06
Michael Eggers <mseggers@protonmail.ch>
2023-01-28 12:54:38 +0100

    Add lclbase component for using lcl graphics stuff

--- meSDL2/TextureManager.pas
+++ meSDL2/TextureManager.pas
@@ -2,15 +2,20 @@ unit TextureManager;
 
 
 interface
-	uses sdl2;
+	uses Graphics, sdl2;
 
 	function LoadTexture(TextureFile : string) : PSDL_Texture;
 
 
 implementation
 	function LoadTexture(TextureFile : string) : PSDL_Texture;
+	var
+		ImageData : TPicture;
 	begin
+		ImageData.Create;
+		ImageData.LoadFromFile('Test.png');
 		WriteLn('LoadTexture: loading file ', TextureFile);
+
 		LoadTexture := nil;
 	end;
 
