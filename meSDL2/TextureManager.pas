c286d80aecb33f2f5aa2242e3ddbdf88c7f4dfac
Michael Eggers <mseggers@protonmail.ch>
2023-01-30 01:13:48 +0100

    Texture Exception more meaningful

--- meSDL2/TextureManager.pas
+++ meSDL2/TextureManager.pas
@@ -13,14 +13,14 @@ implementation
 		ImageData.Create;
 		try	
 			ImageData.LoadFromFile('Test.png');
+			WriteLn('LoadTexture: loading file ', TextureFile);
 		except
 			begin
 				WriteLn('Could not load file Test.png');
 				// Load default texture or something...
 			end;
 		end;
-		WriteLn('LoadTexture: loading file ', TextureFile);
-
+		
 		LoadTexture := nil;
 	end;
 
